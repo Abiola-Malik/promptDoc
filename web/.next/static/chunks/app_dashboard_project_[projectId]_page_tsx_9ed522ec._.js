b08d9977fd4cb56af6268c8178ab89fef9dd5dce
abiola <abiolamalik30@gmail.com>
2026-06-09 16:27:35 +0100

    chore(infra): remove .next from tracking, add CI pipeline

--- web/.next/static/chunks/app_dashboard_project_[projectId]_page_tsx_9ed522ec._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_dashboard_project_[projectId]_components_3e45be40._.js",
-  "static/chunks/node_modules_cb3c1fbe._.js",
-  "static/chunks/_960d4535._.js"
-],
-    source: "dynamic"
-});
